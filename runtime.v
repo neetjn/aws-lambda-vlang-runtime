abb3a5b8667911f10c406a653b44f254f0b93589
John Nolette <john.nolette@messagebird.com>
2022-10-23 19:48:16 -0400

    Adding runtime struct funcs

--- runtime.v
+++ runtime.v
@@ -1,26 +1,68 @@
-/*
-private $url;
-	private $functionCodePath;
-	private $requestId;
-	private $response;
-	private $rawEventData;
-	private $eventPayload;
-	private $handler;
-*/
+import http
 
 GET := 'GET'
 POST := 'POST'
 
 struct LambdaRuntime {
-  url string
-  functionCodePath string
-  response string
+  mut url string
+  mut functionCodePath string
+  mut response string
   // TODO: investigate struct for event data
-  rawEventData string
+  mut rawEventData Response
   // TODO: investigate struct for event payload
-  eventPayload string
+  mut eventPayload string
   // TODO: investigate fn for handler
-  handler string
+  mut handler string
 }
 
-// fn init
+fn (rt LambdaRuntime) resetResponse () {
+  rt.response = ""
+}
+
+fn (rt LambdaRuntime) addToResponse (response) {
+  this.response = response
+}
+
+fn (rt LambdaRuntime) flushResponse () {
+  requestId := rt.requestId
+  response := rt.response
+  http.post('/2018-06-01/runtime/invocation/$requestId/response', response)
+  rt.resetResponse()
+}
+
+fn (rt LambdaRuntime) getNextEventData () ?Response {
+  rt.rawEventData = http.get('/2018-06-01/runtime/invocation/next')
+  eventData := rt.rawEventData.text
+  requestId := rt.rawEventData.header.data['lambda-runtime-aws-request-id'][0]
+  if (!requestId) {
+    rt.reportError(
+      'MissingEventData',
+      'Event data is absent. EventData: $eventData'
+    )
+    return {
+      error: true,
+    }
+  }
+  rt.requestId = requestId
+  rt.eventPayload = eventData
+  return rt.rawEventData
+}
+
+fn (rt LambdaRuntime) reportError (errorType string, errorMessage string) {
+  requestId := rt.requestId
+  errorArray := {
+    errorType,
+    errorMessage,
+  }
+  errorPayload := ""
+  http.post('/2018-06-01/runtime/invocation/$requestId/error', errorPayload)
+}
+
+fn (rt LambdaRuntime) reportInitError (errorType string, errorMessage string) {
+  errorArray := {
+    errorType,
+    errorMessage,
+  }
+  errorPayload := ""
+  http.post('/2018-06-01/runtime/init/error', errorPayload)
+}
