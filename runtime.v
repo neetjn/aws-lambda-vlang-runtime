4d59da36c8b2a57ab572566bedda1ac1210ccdb8
John Nolette <john.nolette@messagebird.com>
2022-10-23 18:03:11 -0400

    Setting up structs

--- runtime.v
+++ runtime.v
@@ -1,3 +1,26 @@
-struct LambdaRuntime {
+/*
+private $url;
+	private $functionCodePath;
+	private $requestId;
+	private $response;
+	private $rawEventData;
+	private $eventPayload;
+	private $handler;
+*/
 
+GET := 'GET'
+POST := 'POST'
+
+struct LambdaRuntime {
+  url string
+  functionCodePath string
+  response string
+  // TODO: investigate struct for event data
+  rawEventData string
+  // TODO: investigate struct for event payload
+  eventPayload string
+  // TODO: investigate fn for handler
+  handler string
 }
+
+// fn init
